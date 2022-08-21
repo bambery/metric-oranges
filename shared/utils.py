2eb1b9877eeb182b864d50b3ff8782cde8102ece
bambery <lwszolek@gmail.com>
2022-08-20 23:30:12 -0500

    add file utils and helpers/constants

---
+++ shared/utils.py
@@ -0,0 +1,9 @@
+import os
+from pathlib import Path
+
+def get_project_root() -> Path:
+    return Path(__file__).parent.parent
+
+def get_inputs_dir() -> Path:
+    root = get_project_root()
+    return root.joinpath("inputs")
